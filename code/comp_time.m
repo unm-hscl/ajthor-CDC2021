6913d17e32cd1b615b980adaeb64656e6cc6ffab
adam <adam.thorpe.g@gmail.com>
2021-03-05 14:56:49 -0700

    Adding computation time figure.

---
+++ code/comp_time.m
@@ -0,0 +1,108 @@
+classdef comp_time < matlab.perftest.TestCase
+% COMP_TIME Performance testing class to measure computation time of the
+% algorithm.
+    
+    properties
+        
+        % Number of sample points.
+        M = 5625;
+        
+        % Variables to hold samples.
+        xs
+        us
+        ys
+        
+        c
+        
+        % Algorithm instantiation.
+        alg
+        
+    end
+    
+    properties (TestParameter)
+        
+        % Range of the number of test points to test. 
+        NumberOfTestPoints = num2cell(1:50);
+        
+    end
+    
+    methods (TestMethodSetup)
+        
+        function setupAlgorithm(testCase)
+            
+            % Sampling time.
+            Ts = 0.25;
+            
+            % System matrices.
+            A = [1 Ts; 0 1];
+            B = [(Ts^2)/2; Ts];
+            
+            % Samples.
+            testCase.xs = [
+                -1 + 2*rand(1, testCase.M);
+                -1 + 2*rand(1, testCase.M);
+                ];
+
+            testCase.us = -1.1 + 2.2*rand(1, size(testCase.xs, 2));
+            
+            W = 0.01*randn(size(testCase.xs));
+
+            testCase.ys = A*testCase.xs + B*testCase.us + W;
+            
+            % Evaluate the cost function at the sample points.
+            testCase.c = vecnorm(testCase.ys);
+            
+            % Instantiate the algorithm.
+            testCase.alg = KernelControl('Lambda', 1/testCase.M^2, ...
+                                            'Sigma', 0.25);
+            
+        end
+        
+    end
+    
+    methods (Test)
+        
+        function testAlgorithmVaryingTestPoints(testCase, NumberOfTestPoints)
+
+            % Generate test points.
+            rng(0);
+            xt = rand(2, NumberOfTestPoints);
+            
+            % Admissible control inputs.
+            ur = linspace(-1, 1, 100);
+            
+            testCase.startMeasuring();
+            
+            testCase.alg.compute(testCase.xs, testCase.us, ...
+                                    xt, ur, ...
+                                    testCase.c);
+                                
+            testCase.stopMeasuring();
+            
+            % Dummy condition.
+            testCase.verifyEqual(1, 1);
+            
+        end
+        
+        function testOptimalAlgorithmVaryingTestPoints(testCase, NumberOfTestInputs)
+
+            % Generate test points.
+            rng(0);
+            xt = rand(2, NumberOfTestPoints);
+            
+            dynamics = @(x, u) A*x + B*u;
+
+            % Compute the results using the optimization based algorithm.
+            alg_opt = OptimalControl();
+            
+            testCase.startMeasuring();
+            
+            alg_opt.compute(dynamics, xt, [-1, 1]);
+            
+            testCase.stopMeasuring();
+            
+            % Dummy condition.
+            testCase.verifyEqual(1, 1);
+        
+    end
+end
